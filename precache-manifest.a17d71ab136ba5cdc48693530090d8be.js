09c868cd864fb3a967be5e6dbe754a1b4ac2ec74
Serhii Mishchenko <serhii.mishchenko@area9.dk>
2023-11-21 18:16:34 +0100

    Updates

---
+++ precache-manifest.a17d71ab136ba5cdc48693530090d8be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d13df593f0455a57b6563acb34ff41",
+    "url": "/react-admin-dashboard/index.html"
+  },
+  {
+    "revision": "8dee6f8ee2822832b183",
+    "url": "/react-admin-dashboard/static/css/main.c722d9b5.chunk.css"
+  },
+  {
+    "revision": "cb0064c69b693aa4fa7d",
+    "url": "/react-admin-dashboard/static/js/2.4669d5ca.chunk.js"
+  },
+  {
+    "revision": "1e7a7346baa31f7b98172df30d46cdba",
+    "url": "/react-admin-dashboard/static/js/2.4669d5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0ff2919d86bf28b5ba",
+    "url": "/react-admin-dashboard/static/js/3.81927bdc.chunk.js"
+  },
+  {
+    "revision": "2ee4a329ded82e8e12215f331bb46922",
+    "url": "/react-admin-dashboard/static/js/3.81927bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53528e176dc1dafdb37a",
+    "url": "/react-admin-dashboard/static/js/4.f28a0c75.chunk.js"
+  },
+  {
+    "revision": "8dee6f8ee2822832b183",
+    "url": "/react-admin-dashboard/static/js/main.815e8375.chunk.js"
+  },
+  {
+    "revision": "fbd1856b6d9c13f52bf7",
+    "url": "/react-admin-dashboard/static/js/runtime-main.24560bea.js"
+  }
+]);
